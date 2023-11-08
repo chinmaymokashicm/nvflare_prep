264116fd9aecf538ecc79b58ee87d896c2de2772
Chinmay Mokashi <chinmaymokashicm@gmail.com>
2023-11-08 09:45:38 -0600

    Added autocast to trainer and fixed progress_plot code

--- trainer.py
+++ trainer.py
@@ -4,7 +4,6 @@ import os
 
 import torch
 torch.cuda.empty_cache()
-print("Current CUDA device:", torch.cuda.current_device())
 import torch.nn as nn
 from torch.utils.data import DataLoader, random_split
 # Load dataset
@@ -24,7 +23,8 @@ class Trainer:
             2: "Heart",
             6: "Lung"
         }
-        data_dir = f"Task{task_num:02d}_{dict_task_num2dataset[task_num]}"
+        dataset = dict_task_num2dataset[task_num]
+        data_dir = f"Task{task_num:02d}_{dataset}"
         folderpath_model = os.path.join("models", f"task{task_num:02d}")
         
         dict_task_num2data_prefix = {
@@ -44,19 +44,16 @@ class Trainer:
         # Initiate logging
         self.init_logging()
         logging.info(f"Task number: {task_num}, dataset: {dict_task_num2dataset[task_num]}")
-        logging.info(f"Beginning training. Hyperparameters: n_epochs: {self.n_epochs}, batch_size: {self.batch_size}, lr: {lr}, data_dir: {data_dir}, num_classes: {num_classes}, validation_split: {validation_split}")
+        logging.info(f"Beginning training. Hyperparameters: n_epochs: {self.n_epochs}, batch_size: {self.batch_size}, lr: {self.lr}, data_dir: {data_dir}, num_classes: {num_classes}, validation_split: {validation_split}")
         
         # Set hyperparameters
         is_cuda_available = torch.cuda.is_available()
         logging.info(f"Is CUDA available? {'Yes' if is_cuda_available else 'No'}")
-        # if is_cuda_available:
-        #     self.device = torch.device("cuda:2")
-        # else:
-        #     self.device = torch.device("cpu")
-        self.device = torch.device("cuda" if is_cuda_available else "cpu")
+        cuda_visible_devices = os.environ.get("CUDA_VISIBLE_DEVICES")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         # Initialize model
-        # self.model = UNet3D(in_channels=1, num_classes=num_classes).to(self.device)
         self.model = UNet3D(in_channels=1, num_classes=num_classes)
+        self.model.to(self.device)
         
         # Define loss and optimizer
         self.criterion = nn.CrossEntropyLoss()
@@ -69,12 +66,12 @@ class Trainer:
         n_samples = len(dataset_full)
         n_val_samples = int(validation_split * n_samples)
         n_train_samples = n_samples - n_val_samples
-        # print("Number of samples:", n_samples, n_val_samples, n_train_samples)
         self.train_dataset, self.val_dataset = random_split(dataset_full, [n_train_samples, n_val_samples])
         
         self.train_loader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
         self.val_loader = DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False)
-        
+        self.train_losses = []
+        self.val_losses = []
     
     def execute(self):
         try:
@@ -86,35 +83,20 @@ class Trainer:
                 for i, batch in enumerate(self.train_loader):
                     logging.info(f"Batch number: {i+1}/{len(self.train_loader)}")
                     inputs, labels = batch
-                    # inputs = inputs.to(self.device)
-                    # labels = labels.to(self.device, dtype=torch.long)
-                    # labels = torch.Tensor.long(labels)
-                    # inputs = torch.as_tensor(inputs, dtype=torch.long)
-                    labels = torch.as_tensor(labels, dtype=torch.long)
-                    # labels = torch.as_tensor(labels, dtype=torch.float64)
-                    print("Converted input and labels to tensors")
-                    print("inputs:", inputs.size(), type(inputs), inputs.dtype)
-                    print("labels:", labels.size(), type(labels), labels.dtype)
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device, dtype=torch.long)
                     self.optimizer.zero_grad()
-                    print("Done zero grad")
-                    outputs = self.model(inputs)
-                    print("Received output")
-                    loss = self.criterion(outputs, labels)
-                    print("Calculated loss")
-                    # print(loss)
-                    # print(type(loss))
-                    print(loss.size())
+                    with torch.autocast(device_type="cuda"):
+                        outputs = self.model(inputs)
+                        loss = self.criterion(outputs, labels)
                     gc.collect()
                     loss.backward()
-                    print("Performed backpropagation")
                     self.optimizer.step()
-                    print("Step optimizer")
                     total_loss += loss.item()
-                    print("Calculated total loss")
                 
                 average_loss = total_loss / (i + 1)
-                print("Calculated average loss over all batches")
                 logging.info(f"Epoch [{epoch + 1}/{self.n_epochs}] - Training Loss: {average_loss:.4f}")
+                self.train_losses.append(average_loss)
 
                 # Validation loop
                 self.model.eval()
@@ -123,25 +105,37 @@ class Trainer:
                 with torch.no_grad():
                     for i, batch in enumerate(self.val_loader):
                         inputs, labels = batch
-                        # inputs = inputs.to(self.device)
-                        # labels = labels.to(self.device)
+                        inputs = inputs.to(self.device)
+                        labels = labels.to(self.device, dtype=torch.long)
                         val_outputs = self.model(inputs)
                         val_loss = self.criterion(val_outputs, labels)
                         total_val_loss += val_loss.item()
 
                 average_val_loss = total_val_loss / (i + 1)
                 logging.info(f"Epoch [{epoch + 1}/{self.n_epochs}] - Validation Loss: {average_val_loss:.4f}")
+                self.val_losses.append(average_val_loss)
+            
+                # Plot and save the training and validation loss
+                self.plot_progress(os.path.join(self.folderpath_model, "progress_plot.png"))
+                logging.info(f"Progress plot saved to {os.path.join(self.folderpath_model, 'progress_plot.png')}")
 
-            # Save the model after all epochs if needed
+            # Save the model after all epochs
             torch.save(self.model.state_dict(), f"{self.folderpath_model}/model_final.pt")
             logging.info(f"Model saved to {self.folderpath_model}")
-            
-            # Plot and save the training and validation loss
-            self.plot_progress(os.path.join(self.folderpath_model, "progress_plot.png"))
-            logging.info(f"Progress plot saved to {os.path.join(self.folderpath_model, 'progress_plot.png')}")
         except Exception:
             logging.error(traceback.format_exc())
-            # logging.exception("Error in training process.", exc_info=True)
+            
+    def plot_progress(self, plot_filename):
+        epochs = list(range(1, len(self.train_losses) + 1))
+        plt.figure()
+        plt.plot(epochs, self.train_losses, label='Training Loss')
+        plt.plot(epochs, self.val_losses, label='Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Training and Validation Loss Progress')
+        plt.legend()
+        plt.savefig(plot_filename)
+        plt.close()
         
     def init_logging(self):
         log_format = "%(asctime)s [%(levelname)s]: %(message)s"
